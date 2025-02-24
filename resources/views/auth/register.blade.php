44422182327e3b40154de89dae284b9eb829374e
NICOLAS ROCK PESSOA <60918958342@icc.local>
2025-02-24 15:18:35 -0300

    Alteração em icon da Barra

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('images/Barra.png') }}" type="image/x-icon">
     <title>Cadastro</title>
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
